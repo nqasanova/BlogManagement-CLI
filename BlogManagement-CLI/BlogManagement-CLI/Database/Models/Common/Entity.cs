c779ddc81ae723af975546dfa5d0dea7c9d3945b
Natavan Hasanova <natavanhesenova4@gmail.com>
2024-02-17 21:12:12 +0400

    Add base class for common database models.

--- BlogManagement-CLI/BlogManagement-CLI/Database/Models/Common/Entity.cs
+++ BlogManagement-CLI/BlogManagement-CLI/Database/Models/Common/Entity.cs
@@ -1,6 +1,8 @@
+using System;
 namespace BlogManagement_CLI.Database.Models.Common;
 
-public class Entity
+public abstract class Entity<TId>
 {
-    
+    public TId Id { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
